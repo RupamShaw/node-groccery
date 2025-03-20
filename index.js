d1280d25aaaf6507187055e0befb2d92ad151d17
rupamshaw <rupam.shaw@gmail.com>
2025-03-20 18:00:34 +1100

    express

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const fastify = require("fastify")()
+// const fastify = require("fastify")()
 
-fastify.get("/", async (request, reply) => {
-  return {message: "Hello, World!"}
+// fastify.get("/", async (request, reply) => {
+//   return {message: "Hello, World!"}
+// })
+// // const PORT = process.env.PORT || 4000
+// fastify.listen({port: 8000}, () => console.log("Server running on port "))
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT || 4000 // Use Azure's assigned port or default to 4000
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!")
 })
-const PORT = process.env.PORT || 4000
-fastify.listen({port: PORT}, () => console.log("Server running on port ", PORT))
-// const express = require("express")
-// const app = express()
 
-// const PORT = process.env.PORT || 4000 // Use Azure's assigned port or default to 4000
-
-// app.get("/", (req, res) => {
-//   res.send("Hello, World!")
-// })
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
