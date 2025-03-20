b71383baf7c5c9f95ea0bad40d4e61b61da6649a
rupamshaw <rupam.shaw@gmail.com>
2025-03-20 15:59:53 +1100

    port 8080

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ fastify.get("/", async (request, reply) => {
   return {message: "Hello, World!"}
 })
 // const PORT = process.env.PORT || 4000
-fastify.listen({port: 8000}, () => console.log("Server running on port "))
+fastify.listen({port: 8080}, () => console.log("Server running on port "))
